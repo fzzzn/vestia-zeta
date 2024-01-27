3258f23493c3e65e2f94071cab9c465908a17f35
fzzzn <fauzan@zxn.my.id>
2024-01-27 20:21:06 +0700

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+const img = document.getElementById("zeta-image");
+const text = document.getElementById("zeta-image-text");
+
+function zetaV1() {
+  img.src = "/assets/images/zeta-v1.png";
+  text.innerText = "Vestia Zeta V1";
+}
+
+function zetaV2() {
+  img.src = "/assets/images/zeta-v2.png";
+  text.innerText = "Vestia Zeta V2";
+}
+
+function zetaV3() {
+  img.src = "/assets/images/zeta-v3.png";
+  text.innerText = "Vestia Zeta V3";
+}
